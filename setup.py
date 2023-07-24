1f27841ea10440cce29416dcb418fba9fa664560
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-07-24 14:58:13 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="youtube00e1f862e5eff",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     install_requires=[
         "lxml",

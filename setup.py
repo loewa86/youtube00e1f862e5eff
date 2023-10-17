9d737202fb5501b63a072ef2c1aaa45aed48c5f6
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-10-17 11:30:02 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import find_packages, setup
 
 setup(
     name="youtube00e1f862e5eff",
-    version="0.0.37",
+    version="0.0.38",
     packages=find_packages(),
     install_requires=[
         "lxml",
         "exorde_data",
         "aiohttp",
-        "nltk==3.8.1",
+        "nltk==3.6.2",
         "dateparser>=1.1.3",
         "requests>=2.27.1",
         "HTMLParser"

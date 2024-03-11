df7347a3e2501f6d8b35a2fce9b535113fa7c561
patrick borowy <patrick.borowy@protonmail.com>
2024-03-11 17:16:41 +0100

    fix: add missing beautifulsoup4 dependency

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="youtube00e1f862e5eff",
-    version="0.0.45",
+    version="0.0.46",
     packages=find_packages(),
     install_requires=[
         "lxml",
@@ -11,7 +11,8 @@ setup(
         "nltk==3.6.2",
         "dateparser>=1.1.3",
         "requests>=2.27.1",
-        "HTMLParser"
+        "HTMLParser",
+        "beautifulsoup4>=4.11"
     ],
     extras_require={"dev": ["pytest", "pytest-cov", "pytest-asyncio"]},
 )
